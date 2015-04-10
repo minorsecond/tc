9233ce251af6ff12802a83df6cc0aa067a969533
Adam Smith <adsmith@nottheeconomist.com>
2015-04-10 16:58:27 -0700

    Partially implemented Timer as a thread. Complete this commit when I get home

--- tc.py
+++ tc.py
@@ -14,6 +14,8 @@ import os.path
 import select
 import logging
 import uuid
+import threading
+import collections
 
 LOGFILE = "timeclock.log"
 FORMATTER_STRING = r"%(levelname)s :: %(asctime)s :: in " \
@@ -23,6 +25,69 @@ logging.basicConfig(filename=LOGFILE, format=FORMATTER_STRING, level=LOGLEVEL)
 
 sumtime = 0
 project_time = 0
+
+class Timer(threading.Thread):
+    """Timer thread to track job timing
+
+    initialized with `jobname` and `abbr` fields, the timer will keep running
+    until you you call `the_timer.q.put("POISON")`
+
+    Timer.seconds -> int
+        how many seconds have passed since timer start
+        this respects time paused!
+    Timer.time_paused -> int
+        how much time has been spend paused, in seconds
+    Timer.pause(reason) -> None
+        pauses the timer
+    Timer.unpause -> None
+        unpauses the timer
+    """
+
+    def __init__(self, jobname, abbr, *args, **kwargs):
+        super(Timer, self).__init__(args, kwargs)
+        self.q = collections.Queue()
+        self.start_time = datetime.datetime.now()
+        self.pause_times = []
+        self.jobname = jobname
+        self.abbr = abbr
+        self.paused = False
+
+    @property
+    def time_paused(self):
+        return sum((pause.get('stop', datetime.datetime.now()) -
+                    pause['start']).total_seconds() for
+                    pause in self.pause_times)
+
+    @property
+    def seconds(self):
+        now = datetime.datetime.now()
+        return (now - self.start_time).total_seconds() - time_paused
+
+    def pause(self, reason):
+        if self.paused:
+            msg = "Can't pause {!r} since it's already paused!".format(self))
+            logging.error(msg)
+            raise ValueError(msg)
+        p = {'start': datetime.datetime.now(),
+             'reason': reason}
+        self.pause_times.append(p)
+        self.paused = True
+
+    def unpause(self):
+        if not self.paused:
+            msg = "Can't unpause {!r} since it's not paused!".format(self)
+            logging.error(msg)
+            raise ValueError(msg)
+        p = self.pause_times[-1]
+        p['stop'] = datetime.datetime.now()
+        self.paused = False
+
+    def run(self):
+        while True:
+            if self.q.get_nowait() == "POISON":
+                return self.seconds
+            # else pass
+
 # CSV columns
 columns = ["Date", "Day Start", "Project Abbrev", "Project Name",
            "Project Start", "Project End", "Time Out", "Time In",
