6c2bf33cfc1cf6ab42fd0b4bf1e0426e79f742a7
Robert Ross Wardrup <minorsecond@gmail.com>
2015-04-18 07:43:35 -0500

    Revert "Sqlalchemy convert"

--- update_metadata.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from models import Employee, Job, Clocktime, Base
-from tc import DB_NAME
-
-engine = create_engine('sqlite:///{}'.format(DB_NAME))
-
-Base.metadata.create_all(engine)
